8b9ed561397d3b9507c4541c7393864dab6cfc38
hohyenho <75921113+hohyenho@users.noreply.github.com>
2025-01-23 11:40:12 +0900

    3번째 commit.

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
 
+import about from "../assets/img/about.png";
+import about2 from "../assets/img/about2.png";
+
+const introText={
+    titel:"port developer",
+    desc:["talent is","found at the end of the","effort"]
+}
+
 const Intro = () => {
   return (
-    <div>Intro</div>
+      <section id='intro'>
+        <div className="intro_inner">
+            <h1 className='intro_title'>
+                {introText.titel}
+            </h1>
+            <div className="intro_lines" aria-hidden="true">
+                <span className='line'></span>
+                <span className='line'></span>
+                <span className='line'></span>
+                <span className='line'></span>
+                <span className='line'></span>
+                <span className='line'></span>
+                <span className='line'></span>
+            </div>
+            <div className="intro_text">
+                <div className="text">
+                    <div>{introText.desc[0]}</div>
+                    <div>{introText.desc[1]}</div>
+                    <div>{introText.desc[2]}</div>
+                </div>
+                <div className="img">
+                    <img src={about} alt="이미지 넣기" />
+                </div>
+            </div>
+            <div className="intro_lines bottom" aria-hidden="true">
+            <span className='line'></span>
+                <span className='line'></span>
+                <span className='line'></span>
+                <span className='line'></span>
+                <span className='line'></span>
+                <span className='line'></span>
+                <span className='line'></span>
+            </div>
+        </div>
+      </section>
   )
 }
 
